6575bc5e5d24e7f0e276d19d170d8a2499af5935
Your Name <you@example.com>
2017-06-20 08:56:04 -0400

    fixed issue in script and add verbosity in crontab

--- client/check_client.sh
+++ client/check_client.sh
@@ -3,5 +3,6 @@ tt_user="admin"
 tt_password="admin"
 tt_server="10.0.0.20"
 tt_port="8000"
+my_hostname=`hostname -f`
 
-curl -X POST  -u ${tt_user}:${tt_password} -d ServerFQDN=`hostname -f`  http://${tt_server}:${tt_port}/status/
+curl -X POST -i -H "Accept: application/json"  -u ${tt_user}:${tt_password} -d '{"ServerFQDN":"'+${my_hostname}+'"}'  http://${tt_server}:${tt_port}/status/ >> /var/log/heartbeat.log
