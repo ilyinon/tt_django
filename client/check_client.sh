3ff4fcc360de77e20c0bdd867d9d15c1d8ba6331
Your Name <you@example.com>
2017-06-20 06:21:35 -0400

    added right IP addess and cred for client accessing

--- client/check_client.sh
+++ client/check_client.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+tt_user="admin"
+tt_password="admin"
+tt_server="10.0.0.20"
+tt_port="8000"
 
-echo "test message"
+curl -X POST  -u ${tt_user}:${tt_password} -d ServerFQDN=`hostname -f`  http://${tt_server}:${tt_port}/status/
