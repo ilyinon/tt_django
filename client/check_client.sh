0e822d451493a368378e165249bacadade8f10bf
Your Name <you@example.com>
2017-06-20 09:46:34 -0400

    fixed misstypes

--- client/check_client.sh
+++ client/check_client.sh
@@ -5,4 +5,4 @@ tt_server="10.0.0.20"
 tt_port="8000"
 my_hostname=`hostname -f`
 
-curl -X POST -i -H "Accept: application/json"  -u ${tt_user}:${tt_password} -d '{"ServerFQDN":"'+${my_hostname}+'"}'  http://${tt_server}:${tt_port}/status/ >> /var/log/heartbeat.log
+curl -X POST -i -H "Accept: application/json"  -u ${tt_user}:${tt_password} -d '{"ServerFQDN":"'${my_hostname}'"}'  http://${tt_server}:${tt_port}/status/ >> /var/log/heartbeat.log
