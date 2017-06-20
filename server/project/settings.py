fc91ee634a9faa558dce18b50b9ad79d5efa8b45
Your Name <you@example.com>
2017-06-20 04:37:37 -0400

    changed backed to postgres

--- server/project/settings.py
+++ server/project/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tt',
+        'USER' : 'postgres',
+        'PASSWORD' : 'postgres',
+        'HOST' : '10.0.0.10',
+        'PORT' : '5432',
     }
 }
 
