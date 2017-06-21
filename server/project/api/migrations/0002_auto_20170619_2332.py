3a6eb465ef230596b20940eef2b58aa0d99bc0b2
Your Name <you@example.com>
2017-06-21 02:22:21 -0400

    merged migration from two files

--- server/project/api/migrations/0002_auto_20170619_2332.py
+++ server/project/api/migrations/0002_auto_20170619_2332.py
@@ -3,13 +3,27 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
+from django.contrib.auth.hashers import make_password
+
+
+def create_admin_user(apps, schema_editor):
+     User = apps.get_registered_model('auth', 'User')
+     admin = User(
+         username='admin',
+         email='admin@localhost',
+         password=make_password('admin'),
+         is_superuser=True,
+         is_staff=True
+     )
+     admin.save()
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('api', '0001_initial'),
-    ]
+        ('auth', '0001_initial'),
+     ]
 
     operations = [
         migrations.DeleteModel(
@@ -22,4 +36,8 @@ class Migration(migrations.Migration):
         migrations.DeleteModel(
             name='Uptime',
         ),
+        migrations.RunPython(create_admin_user),
     ]
+
+
+
